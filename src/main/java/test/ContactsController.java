c3f4e565bf7794ec5577c1fbc5f28f645a349e6a
darkeagle_bb <darkeagle.mail@gmail.com>
2016-12-04 19:44:17 +0200

    final

--- src/main/java/test/ContactsController.java
+++ src/main/java/test/ContactsController.java
@@ -1,8 +1,7 @@
 package test;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.time.LocalTime;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -16,7 +15,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletOutputStream;
@@ -26,9 +24,7 @@ import javax.servlet.http.HttpServletResponse;
 public class ContactsController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private Pattern pattern;
-    int mb = 1024 * 1024;
-    Runtime instance = Runtime.getRuntime();
-
+    private final long LIM = 200000;
 
     @Autowired
     private ContactRepository contactRepository;
@@ -36,7 +32,7 @@ public class ContactsController {
     @RequestMapping("/hello/contacts")
     public ResponseEntity<List<Contact>> contacts(@RequestParam(value="nameFilter", defaultValue="") String nameFilter,
                                                   HttpServletResponse response) {
-        logger.debug("Used memory: \"{}\"", (instance.totalMemory() - instance.freeMemory()) / mb);
+        long time = System.currentTimeMillis();
 
         if (logger.isDebugEnabled()) {
             logger.debug("nameFilter: \"{}\"", nameFilter);
@@ -50,31 +46,40 @@ public class ContactsController {
             }
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+
+        ContactsFilter contactsFilter = new ContactsFilter();
+        contactsFilter.setFilter(pattern);
+        long cnt = contactRepository.getCount();
+        long pos = 0;
         try {
             ServletOutputStream os = response.getOutputStream();
             ObjectMapper mapper = new ObjectMapper();
-            long cnt = contactRepository.getCount();
-            long pos = 0;
-            long lim = 1000000;
+            os.write('[');
             while (pos < cnt) {
-                ContactsFilter contactsFilter = new ContactsFilter(
-                            new LinkedList<>(contactRepository.findInRange(pos, lim)));
-                contactsFilter.setFilter(pattern);
+                contactsFilter.setContacts(new LinkedList<>(contactRepository.findInRange(pos, LIM)));
                 contactsFilter.applyFilter();
-                pos += lim;
+                pos += LIM;
                 if (contactsFilter.getContacts().isEmpty()) {
                     continue;
                 }
                 String jsonInString = mapper.writeValueAsString(contactsFilter.getContacts());
+                jsonInString = jsonInString.substring(1,jsonInString.length()-1);
                 os.write(jsonInString.getBytes());
                 os.flush();
             }
+            os.write(']');
+            os.flush();
+            os.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            if (logger.isDebugEnabled()) {
+                logger.debug(e.getMessage());
+            }
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        logger.debug("Used memory: \"{}\"", (instance.totalMemory() - instance.freeMemory()) / mb);
+        if (logger.isDebugEnabled()) {
+            logger.debug("Exec time: {} ms", System.currentTimeMillis() - time);
+            logger.debug("Response 200 OK");
+        }
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
