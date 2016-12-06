35efaec48d85d1dc6773787be3c8bff339d61c89
darkeagle_bb <darkeagle.mail@gmail.com>
2016-12-07 01:11:35 +0200

    change text

--- src/main/java/test/HomeController.java
+++ src/main/java/test/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     @ResponseBody
-    public String contacts() {
-        return "Hello task 2";
+    public String home() {
+        return "Hello task 2. Proper path is /hello/contacts?nameFilter=[regexp]";
     }
 }
